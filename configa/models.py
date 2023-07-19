3932d9f257d8643cea36c36f028e01aab5ed42be
João Costa <joao21pedro21.costa@gmail.com>
2023-07-19 16:53:04 -0300

    Initial commit

---
+++ configa/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+# Create your models here.
+
+class Categoria(models.Model):
+    nomeCategoria = models.CharField(max_length=50)
+    def __str__(self):
+        return self.nomeCategoria
+
+class Produto(models.Model):
+    nomeProduto = models.CharField(max_length=50)
+    descricaoProduto = models.CharField(max_length=200)
+    precoProduto = models.DecimalField(max_digits=4, decimal_places=2)
+    categoriaProduto = models.ForeignKey(Categoria, on_delete=models.CASCADE)
+    def __str__(self):
+        return self.nomeProduto
+    class Meta:
+        verbose_name_plural = 'Produtos'
